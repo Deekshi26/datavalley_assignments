3df2a87d75a8fa7e7634d7c412009ffcd4015a25
Deekshi26 <163243746+Deekshi26@users.noreply.github.com>
2024-04-01 20:16:24 +0530

    Add files via upload

---
+++ Assignment-5/assignment5.java
@@ -0,0 +1,54 @@
+CHAPTER - 1
+INTRODUCTION
+1.1 PROBLEM STATEMENT
+CHAPTER - 2  
+LITERATURE SURVEY
+CHAPTER 3
+METHODOLOGY
+3.1 Overall Description of the Project:
+3.2 Dataset Description:
+3.3 Requirement Analysis
+   3.3.1	Functional Requirements
+   3.3.1.2	Non-Functional Requirements
+3.4 Implementation Tools and Analysis
+   3.4.1.1	Python 
+   3.4.1.2	Machine Learning
+3.5 Solution Approach
+EXISTING SYSTEM
+PROPOSED SYSTEM
+ADVANTAGES
+DISADVANTAGES
+APPLICATIONS
+
+CHAPTER - 4
+ IMPLEMENTATION 
+4.1 Control Flow Diagram
+4.2 Implementation Details and Issues:
+4.3 Algorithms:
+  4.3.1 SVM
+  4.3.2 Random Forest:
+  4.3.3   Logistic Regression:
+  4.3.4 Decision Trees:
+
+CHAPTER 5
+INTRODUCTION TO PYTHON
+
+CHAPTER 6
+INTRODUCTION TO MACHINE LEARNING
+
+CHAPTER 7
+TESTING
+7.1	Unit Testing: 
+7.2	Integration Testing: 
+7.3	Functional Testing: 
+
+CHAPTER 8
+RESULT AND ANALYSIS
+
+CHAPTER  9
+FINDINGS, CONCLUSIONS, AND FUTURE WORK
+9.1 Findings
+9.2 Conclusions
+9.3 Future Work
+
+
